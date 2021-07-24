d352bdb08a878d3421d1fa2109c8c793f7f2652c
Álvaro Basílio de Araújo <alvaro.araujo@aluno.ufop.edu.br>
2021-07-24 17:28:23 -0300

    change api name

--- src/components/service/productsBelow10.ts
+++ src/components/service/productsBelow10.ts
@@ -1,6 +1,6 @@
 export default async function getProductsBelow() {
-  const result = fetch("http://localhost:3000/api/ship").then((response) =>
-    response.json()
+  const result = fetch("http://localhost:3000/api/productsBelow").then(
+    (response) => response.json()
   );
   return result;
 }
--- src/components/service/productsFree.ts
+++ src/components/service/productsFree.ts
@@ -1,6 +1,6 @@
 export default async function getProductsFree() {
-  const result = fetch("http://localhost:3000/api/freeShip").then((response) =>
-    response.json()
+  const result = fetch("http://localhost:3000/api/productsMore").then(
+    (response) => response.json()
   );
   return result;
 }
